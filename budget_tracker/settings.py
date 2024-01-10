b33831ad2118e3a958fbc3fd5c086c1c63923802
Omry Sinwany <Omrysinwanyy@gmail.com>
2024-01-10 02:50:14 +0200

    com

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourdomain.com', 'sub.yourdomain.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['omrysin.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
