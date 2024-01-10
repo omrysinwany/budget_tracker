704dbd642324566f1ecb440ef4f7cea38f261fbf
Omry Sinwany <Omrysinwanyy@gmail.com>
2024-01-10 03:31:37 +0200

    update settings

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['omrysin.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['omrysin.pythonanywhere.com', 'budget-tracker-j3fu.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
